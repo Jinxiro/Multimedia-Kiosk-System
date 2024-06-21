591b2134160a6c0a354ef74d47e1756b42f5f8b5
unknown <jammilitar@gmail.com>
2024-06-21 14:02:06 +0800

    updated

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -6,12 +6,15 @@ import Navigation from '../components/Navigation.vue'
     <main>
     <Navigation />
     <Header />
-    <div class="ocean">
-  <div class="wave"></div>
-  <div class="wave"></div>
-</div>
     <!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#a2d9ff" fill-opacity="1" d="M0,32L48,58.7C96,85,192,139,288,138.7C384,139,480,85,576,64C672,43,768,53,864,80C960,107,1056,149,1152,165.3C1248,181,1344,171,1392,165.3L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg> -->
-</main>
+  </main>
+  <section>
+    <div class="wave wave1"></div>
+    <div class="wave wave2"></div>
+    <div class="wave wave3"></div>
+  </section>
+
+    
 </template>
 <style scoped>
     *{
@@ -28,7 +31,7 @@ import Navigation from '../components/Navigation.vue'
         display: flex;
         justify-content: center;
         align-items: center;
-        min-height: 95vh;
+        min-height: 90vh;
       svg{
         bottom: 0;
         z-index: -10;
@@ -36,53 +39,36 @@ import Navigation from '../components/Navigation.vue'
         display: flex;
       }
     }
+    section{
+      outline: 1px solid 1px;
+      z-index: -99;
+      width: 100%;
+      overflow: hidden;
+      .wave{
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100px;
+        background: url("/src/assets/images/wave.png");
+        background-size: 1000px 200px;
+      }
+      .wave.wave1{
+        animation: animate 30s linear infinite;
+        z-index: 1000;
+        opacity: 1;
+        animation-delay: 0s;
+        bottom: 0; 
+      }
+      @keyframes animate {
+        0%{
+          background-position-x: 0;
+        }
+        100%{
+          background-position-x:  1000px;
+        }
+      }
+    }
     
-html, body { height: 100%; }
-body {
-  background:radial-gradient(ellipse at center, rgb(255, 242, 0) 0%, rgba(255,254,234,1) 35%, #000000 100%);
-  overflow-y: none;
-}
 
-.ocean { 
-  height: 5%;
-  width:100%;
-  position:absolute;
-  bottom:0;
-  left:0;
-  background: #ffffff;
-}
-
-.wave {
-  background: url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/85486/wave.svg) repeat-x; 
-  position: absolute;
-  top: -198px;
-  width: 5000px;
-  height: 198px;
-  animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) infinite;
-  transform: translate3d(0, 0, 0);
-}
-
-.wave:nth-of-type(2) {
-  top: -175px;
-  animation: wave 7s cubic-bezier( 0.36, 0.45, 0.63, 0.53) -.125s infinite, swell 7s ease -1.25s infinite;
-  opacity: 1;
-}
-
-@keyframes wave {
-  0% {
-    margin-left: 0;
-  }
-  100% {
-    margin-left: -1600px;
-  }
-}
-
-@keyframes swell {
-  0%, 100% {
-    transform: translate3d(0,-25px,0);
-  }
-  50% {
-    transform: translate3d(0,5px,0);
-  }
-}
 </style>
